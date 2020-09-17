b34ad6aa704d365081fb9c9aaa048b1661e0c802
Michael Rocha Leles <rocha_michael@live.com>
2020-09-17 17:59:10 -0300

    Pesquisando e Gravando
    Acerto para pesquisar e gravar .

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,29 +4,38 @@ import whatsappIcon from '../../assests/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {    
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;    
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
 
         <article className="teacher-item">
             <header>
-                <img src="https://pbs.twimg.com/profile_images/950150931/OQAAAEfCZCPN8qr1SPwJ81Ffp28WL56qyjjSdwjHkDALhZDl7kJE_e9Hsls0E0eIp9Q3_e3xbqMZ1Npk1N8XNYddgJsAm1T1UKayV4nrOj3VGh8ezS1g4UI-dli3_normal.jpg" alt="Michael Rocha Leles"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Michael Rocha Leles</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta dos melhores tecnologias de química avançada. Que vai mudar a sua vida.
-                <br />
-                <br />
-                Apaixonado por explodir as coisas.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/Hora
-                    <strong>R$ 80,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
                 <button type="button">
                     <img src={whatsappIcon} alt="WhatsAppMichael"/>
